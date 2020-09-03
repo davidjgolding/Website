368c13973eca4bcfa13e33ad79dae2044a0ac017
David Golding <43792102+davidjgolding@users.noreply.github.com>
2020-09-03 20:43:15 +0100

    Updates

---
+++ precache-manifest.3a3a78d70e2033a52cc562872528adcf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8848e043419764e77960225e5f745855",
+    "url": "/Website/index.html"
+  },
+  {
+    "revision": "47f0e622661a53994974",
+    "url": "/Website/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "7ccc1dab494d2e65a1ed",
+    "url": "/Website/static/css/main.2a5e6669.chunk.css"
+  },
+  {
+    "revision": "47f0e622661a53994974",
+    "url": "/Website/static/js/2.e036731e.chunk.js"
+  },
+  {
+    "revision": "8f2a519a20451a7a97d6b5414ee4ac61",
+    "url": "/Website/static/js/2.e036731e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ccc1dab494d2e65a1ed",
+    "url": "/Website/static/js/main.e1ac4926.chunk.js"
+  },
+  {
+    "revision": "b76995551c5ecba6ae20",
+    "url": "/Website/static/js/runtime-main.ec6b0285.js"
+  }
+]);
