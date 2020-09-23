a9fab8a4dc7410e74f5b202d99f9f0ad6bd8c444
David Golding <43792102+davidjgolding@users.noreply.github.com>
2020-09-23 21:26:57 +0100

    Updates

---
+++ precache-manifest.0de5b8210e428447a6c5ca100c0a2851.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4ae8745b859c3e3145bb8585c0565f3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "79b9b5b37a6c72b4d7c1",
+    "url": "./static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "0b84a495d859d9cb5fa4",
+    "url": "./static/css/main.4bd8f1cd.chunk.css"
+  },
+  {
+    "revision": "79b9b5b37a6c72b4d7c1",
+    "url": "./static/js/2.aca6e571.chunk.js"
+  },
+  {
+    "revision": "8f2a519a20451a7a97d6b5414ee4ac61",
+    "url": "./static/js/2.aca6e571.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b84a495d859d9cb5fa4",
+    "url": "./static/js/main.1dcb5b9d.chunk.js"
+  },
+  {
+    "revision": "2b00759073bc710e4949",
+    "url": "./static/js/runtime-main.1ce6add8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.85ea9846ab9ab18cddfd1f2c5724114f.js"
+  "./precache-manifest.0de5b8210e428447a6c5ca100c0a2851.js"
 );
 
 self.addEventListener('message', (event) => {
